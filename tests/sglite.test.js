0223ec2894c02cee52306e646098593b40cb509f
Chris Stead <cmstead@gmail.com>
2019-12-06 15:50:09 -0800

    Reorganized tests

--- tests/sglite.test.js
+++ tests/sglite.test.js
@@ -14,77 +14,81 @@ describe("SG-Lite core functionality", function () {
 
     describe('isTypeOf', function () {
 
-        const typeExpectations = [
-            {
-                name: 'number',
-                pass: 1234,
-                fail: null
-            },
-            {
-                name: 'string',
-                pass: '1234',
-                fail: null
-            },
-            {
-                name: 'object',
-                pass: {},
-                fail: undefined
-            },
-            {
-                name: 'null',
-                pass: null,
-                fail: undefined
-            },
-            {
-                name: 'undefined',
-                pass: undefined,
-                fail: 'this is a string'
-            },
-            {
-                name: 'boolean',
-                pass: true,
-                fail: undefined
-            },
-            {
-                name: 'bigint',
-                pass: 53n,
-                fail: undefined
-            },
-            {
-                name: 'symbol',
-                pass: Symbol(() => 'test'),
-                fail: undefined
-            },
-        ];
+        describe("Simple types", function () {
+            const typeExpectations = [
+                {
+                    name: 'number',
+                    pass: 1234,
+                    fail: null
+                },
+                {
+                    name: 'string',
+                    pass: '1234',
+                    fail: null
+                },
+                {
+                    name: 'object',
+                    pass: {},
+                    fail: undefined
+                },
+                {
+                    name: 'null',
+                    pass: null,
+                    fail: undefined
+                },
+                {
+                    name: 'undefined',
+                    pass: undefined,
+                    fail: 'this is a string'
+                },
+                {
+                    name: 'boolean',
+                    pass: true,
+                    fail: undefined
+                },
+                {
+                    name: 'bigint',
+                    pass: 53n,
+                    fail: undefined
+                },
+                {
+                    name: 'symbol',
+                    pass: Symbol(() => 'test'),
+                    fail: undefined
+                },
+            ];
 
-        typeExpectations.forEach(function ({ name, pass, fail }) {
-            it(`when called with different values, ${name} check resolves correctly`, function () {
-                const numberCheckResultTrue = sglite.isTypeOf(types[name])(pass);
-                const numberCheckResultFalse = sglite.isTypeOf(types[name])(fail);
+            typeExpectations.forEach(function ({ name, pass, fail }) {
+                it(`when called with different values, ${name} check resolves correctly`, function () {
+                    const numberCheckResultTrue = sglite.isTypeOf(types[name])(pass);
+                    const numberCheckResultFalse = sglite.isTypeOf(types[name])(fail);
 
-                assert.isTrue(numberCheckResultTrue);
-                assert.isFalse(numberCheckResultFalse);
+                    assert.isTrue(numberCheckResultTrue);
+                    assert.isFalse(numberCheckResultFalse);
+                });
+            });
+
+            it('returns true when called with any and a value', function () {
+                const anyCheckResult = sglite.isTypeOf(types.any)('This is a string');
+
+                assert.isTrue(anyCheckResult, 'Any check failed to return true');
             });
         });
+        describe("Parameterized Types", function () {
 
-        it('returns true when called with any and a value', function () {
-            const anyCheckResult = sglite.isTypeOf(types.any)('This is a string');
+            describe("Array", function () {
+                it('when called with different values, array check resolves correctly', function () {
+                    const checkResultTrue = sglite.isTypeOf(types.array)([]);
+                    const checkResultFalse = sglite.isTypeOf(types.array)(null);
 
-            assert.isTrue(anyCheckResult, 'Any check failed to return true');
+                    assert.isTrue(checkResultTrue);
+                    assert.isFalse(checkResultFalse);
+                });
+            });
+
+
+
         });
-
-    });
-
-    describe("Array Type", function () {
-        
-        it('when called with different values, array check resolves correctly', function () {
-            const checkResultTrue = sglite.isTypeOf(types.array)([]);
-            const checkResultFalse = sglite.isTypeOf(types.array)(null);
-
-            assert.isTrue(checkResultTrue);
-            assert.isFalse(checkResultFalse);
-        });
-
     });
 
 });
